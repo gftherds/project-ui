f7dc332331bdb6b3cef716087b45aff18d0e11a3
Nutdanai Prom <Nutdanai.p@augmentis.biz>
2016-10-15 01:29:58 +0700

    YeutSen: Dialog/Notification --> Service,Broadcast,CallBack Splash---> Theme,Style Animation ---> Text ,List Activity one : Fragment many(Back Pressed) etc.

--- app/src/main/java/com/example/therdsak/yeutsen/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/therdsak/yeutsen/MainActivity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.therdsak.yeutsen.MainActivity;
+package com.example.therdsak.yeutsen.mainactivity;
 
 import android.graphics.Color;
 import android.os.Build;
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.Window;
 
 import com.example.therdsak.yeutsen.R;
+import com.example.therdsak.yeutsen.sharedpreference.YeutSenPreference;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -31,32 +32,26 @@ public class MainActivity extends AppCompatActivity {
 
         fm = getSupportFragmentManager();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            // Get Window
-            final Window window = getWindow();
-            // Set Fullscreen
-            window.getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_FULLSCREEN
-            );
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
 
-
-        Log.d(TAG, "onCreate: fragment manager: " + fm);
-         Fragment f = SplashFragment.newInstance();
+        Fragment f = SplashFragment.newInstance();
         fm.beginTransaction().add(R.id.fragment_container, f).commit();
         test();
     }
+
     private void test() {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 //Second fragment after 5 seconds appears
-                Fragment f1 = TutorialFragment.newInstance();
-                fm.beginTransaction().replace(R.id.fragment_container, f1).commit();
+                if(!YeutSenPreference.isTutorailOn(MainActivity.this)) {
+                    Fragment f1 = TutorialFragment.newInstance();
+                    fm.beginTransaction().replace(R.id.fragment_container, f1).commit();
+                    YeutSenPreference.setTutorailOn(MainActivity.this,true);
+                }else{
+                    Fragment f = RegisterFragment.newInstance();
+                    fm.beginTransaction().replace(R.id.fragment_container, f).commit();
+                }
             }
         }, 5000);
 
